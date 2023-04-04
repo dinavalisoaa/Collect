6f7fd6a8f1a44e494653893fc8f87bcdaaa0c4ac
idealy-ma <r.idealy.ma@gmail.com>
2023-04-04 16:41:09 +0300

    Create mobil app

---
+++ mobil-app/android/app/src/main/java/com/idea/smma/MainActivity.java
@@ -0,0 +1,5 @@
+package com.idea.smma;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ mobil-app/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.idea.smma',
+  appName: 'smma',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
