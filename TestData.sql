82116e5edeaf51c19cb9b6c7933acd5f6a2a0509
Mino2809 <99280400+Mino2809@users.noreply.github.com>
2023-04-17 11:48:31 +0300

    Noesorina lay Transportid INT4 NOT NULL, tamin ny table transport

---
+++ TestData.sql
@@ -0,0 +1,65 @@
+INSERT INTO
+    admin
+VALUES
+    (1, 'admin', 'admin', md5('admin'));
+
+INSERT INTO
+    TypeProduit(nom)
+VALUES
+    ('LEGUME');
+
+INSERT INTO
+    TypeProduit(nom)
+VALUES
+    ('FRUIT');
+
+INSERT INTO
+    TypeProduit(nom)
+VALUES
+    ('DIVERS');
+
+INSERT INTO
+    Saison(nom, moisdebut, moisfin)
+VALUES
+    ('HIVER', 6, 9);
+
+INSERT INTO
+    Saison(nom, moisdebut, moisfin)
+VALUES
+    ('PRINTEMPS', 9, 12);
+
+INSERT INTO
+    Saison(nom, moisdebut, moisfin)
+VALUES
+    ('ETE', 12, 3);
+
+INSERT INTO
+    Saison(nom, moisdebut, moisfin)
+VALUES
+    ('AUTOMNE', 3, 6);
+
+INSERT INTO
+    region(nom)
+VALUES
+    ('Analamanga'),
+    ('Itasy'),
+    ('Bongolava'),
+    ('Vakinakaratra'),
+    ('Haute Matsiatra'),
+    ('Sofia'),
+    ('Diana'),
+    ('Sava'),
+    ('Alaotra Mangoro'),
+    ('Betsiboka'),
+    ('Menabe'),
+    ('Androy'),
+    ('Ihorombe'),
+    ('Amoron i Mania'),
+    ('Vatovavy Fitovinanay'),
+    ('Melaky'),
+    ('Analajirofo'),
+    ('Antsinanana'),
+    ('Atsimo Antsinanana'),
+    ('Atsimo Andrefana'),
+    ('Anosy'),
+    ('Boeny');
--- base_baovola.sql
+++ base_baovola.sql
@@ -118,11 +118,11 @@ CREATE TABLE Saison (
     PRIMARY KEY (id)
 );
 
+
 CREATE TABLE Transport (
     id SERIAL NOT NULL,
     nom VARCHAR(255),
     contact VARCHAR(255),
-    Transportid INT4 NOT NULL,
     PRIMARY KEY (id)
 );
 
@@ -237,68 +237,3 @@ ALTER TABLE
 ADD
     COLUMN nom VARCHAR;
 
-INSERT INTO
-    admin
-VALUES
-    (1, 'admin', 'admin', md5('admin'));
-
-INSERT INTO
-    TypeProduit(nom)
-VALUES
-    ('LEGUME');
-
-INSERT INTO
-    TypeProduit(nom)
-VALUES
-    ('FRUIT');
-
-INSERT INTO
-    TypeProduit(nom)
-VALUES
-    ('DIVERS');
-
-INSERT INTO
-    Saison(nom, moisdebut, moisfin)
-VALUES
-    ('HIVER', 6, 9);
-
-INSERT INTO
-    Saison(nom, moisdebut, moisfin)
-VALUES
-    ('PRINTEMPS', 9, 12);
-
-INSERT INTO
-    Saison(nom, moisdebut, moisfin)
-VALUES
-    ('ETE', 12, 3);
-
-INSERT INTO
-    Saison(nom, moisdebut, moisfin)
-VALUES
-    ('AUTOMNE', 3, 6);
-
-INSERT INTO
-    region(nom)
-VALUES
-    ('Analamanga'),
-    ('Itasy'),
-    ('Bongolava'),
-    ('Vakinakaratra'),
-    ('Haute Matsiatra'),
-    ('Sofia'),
-    ('Diana'),
-    ('Sava'),
-    ('Alaotra Mangoro'),
-    ('Betsiboka'),
-    ('Menabe'),
-    ('Androy'),
-    ('Ihorombe'),
-    ('Amoron i Mania'),
-    ('Vatovavy Fitovinanay'),
-    ('Melaky'),
-    ('Analajirofo'),
-    ('Antsinanana'),
-    ('Atsimo Antsinanana'),
-    ('Atsimo Andrefana'),
-    ('Anosy'),
-    ('Boeny');
