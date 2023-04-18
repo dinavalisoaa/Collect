8c24a22a77ae5003b04a025c854023aba7ac80e1
idealy-ma <r.idealy.ma@gmail.com>
2023-04-18 10:21:33 +0300

    ok

--- mobil-app/src/model/User.ts
+++ mobil-app/src/model/User.ts
@@ -14,7 +14,6 @@ export class User {
             db.executeSql(`
                 CREATE TABLE IF NOT EXISTS users (
                     id INTEGER PRIMARY KEY,
-                    name TEXT NOT NULL,
                     email TEXT UNIQUE NOT NULL,
                     motDePasse TEXT UNIQUE NOT NULL
                 )
